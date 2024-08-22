5f3458401fe250c8fc064d347af85ba5c4703518
Noura Bouaissa <nourabouaissa2@gmail.com>
2024-08-22 09:29:18 +0100

    fix buges

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   baseUrl:"http://localhost:8080/api/",
-  token: "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqb2huLmRvZTFAZXhhbXBsZS5jb20iLCJyb2xlcyI6IltDTElFTlRdIiwiZXhwIjoxNzI0MTkwNzE0fQ.wq1fVfUXeszVcOsOiLm12sl4WzVCw5q-nAjh9LmgBXY",
+  token: "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqb2huLmRvZTFAZXhhbXBsZS5jb20iLCJyb2xlcyI6IltDTElFTlRdIiwiZXhwIjoxNzI0MzIyMjg0fQ.q2MRyHVfjyMOTzylsKDppYI3l05ro6GHFe57Orb05Rc",
 };
