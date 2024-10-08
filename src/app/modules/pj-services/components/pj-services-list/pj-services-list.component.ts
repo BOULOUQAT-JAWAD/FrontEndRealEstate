e2eb2e109110fb14d552a5ea2ec36ddf5ea2b6cd
Yassine Belafki <yassine.belafki@oracle.com>
2024-10-08 10:37:11 +0100

    fixed add to card problem

--- src/app/modules/pj-services/components/pj-services-list/pj-services-list.component.ts
+++ src/app/modules/pj-services/components/pj-services-list/pj-services-list.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { PjService } from '../../models/pj-service';
 import { PjServicesService } from '../../services/pj-services.service';
 import { PropertyService } from 'src/app/modules/properties/services/property.service';
 import { PropertyResponse } from 'src/app/modules/properties/models/property-response';
 import { LocalStorageService } from 'ngx-webstorage';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-pj-services-list',
@@ -18,7 +20,9 @@ export class PjServicesListComponent implements OnInit{
   cartItems: { propertyId: string; pjServicesIds: string[] }[] = [];
 
 
-  constructor(private pjServiceService: PjServicesService,private localStorage:LocalStorageService,private propertyService:PropertyService) {}
+  constructor(private pjServiceService: PjServicesService,
+    private localStorage:LocalStorageService,private propertyService:PropertyService
+    ,private cdr: ChangeDetectorRef,private toastr:ToastrService) {}
 
   ngOnInit(): void {
     this.fetchPjServices();
@@ -29,6 +33,7 @@ export class PjServicesListComponent implements OnInit{
       next: (returnedProperties: PropertyResponse[]) => {
         console.log(returnedProperties)
         this.propertiesList = returnedProperties;
+        this.cdr.detectChanges();
         console.log(this.propertiesList)
       },
       error: (err) => {
@@ -81,13 +86,21 @@ export class PjServicesListComponent implements OnInit{
             pjServicesIds: serviceIds
           });
         }
-      
       }
   
       const checkoutData = { propertyServiceCheckouts };
       console.log(checkoutData); // This is where you can send the data to your backend or handle it further.
       this.localStorage.store("cart",JSON.stringify(checkoutData));
+      if (Object.keys(this.selectedProperties).length === 0) {
+        this.toastr.info("vous n'avez choisi aucun service")
+      } else {
+        this.toastr.success("éléments ajoutés à la carte avec succès, pouvez-vous s'il vous plaît vérifier votre carte")
+      }
      // this.cartItems = checkoutData.propertyServiceCheckouts; // Optionally display added items in the cart
+    }
+
+    trackByProperty(index: number, property: any): string {
+      return property.propertyId; // Ensure that the property ID is unique
     }
 
 }
